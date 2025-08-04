9410cd0f2ca232d629d71d08cdcd8e0c0d9c7fc5
naiemjoy1 <naiemunhasan@gmail.com>
2025-08-04 13:28:36 +0600

    first commit

---
+++ module/output/script.js
@@ -0,0 +1,16 @@
+var b;
+var a = [];
+b = "naiem";
+a.push(44, "naiem", true);
+console.log(a);
+console.log(b);
+// object
+var c;
+c = {
+    name: "naiem",
+    age: 34,
+    adult: true,
+};
+console.log(c);
+export {};
+//# sourceMappingURL=script.js.map
