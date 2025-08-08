3b46b65b1a1bbbae23bbc96a7cc5b2cc10fbf2b2
naiemjoy1 <naiemunhasan@gmail.com>
2025-08-08 16:35:07 +0600

    enum

---
+++ module/output/enumType.js
@@ -0,0 +1,15 @@
+var RType;
+(function (RType) {
+    RType[RType["SUCCESS"] = 0] = "SUCCESS";
+    RType[RType["FAILURE"] = 1] = "FAILURE";
+    RType[RType["UNAUTHENTICATED"] = 2] = "UNAUTHENTICATED";
+    RType[RType["FORBIDDEN"] = 3] = "FORBIDDEN";
+})(RType || (RType = {}));
+const response1 = {
+    status: 200,
+    type: RType.UNAUTHENTICATED,
+    data: "test",
+};
+console.log(response1);
+export {};
+//# sourceMappingURL=enumType.js.map
---
+++ module/output/generics.js
@@ -0,0 +1,7 @@
+const response1 = {
+    status: 200,
+    type: "good",
+    data: "test",
+};
+export {};
+//# sourceMappingURL=generics.js.map
