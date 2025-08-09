8ae22357c9722a95afa06ea7e8ba9b68bf9e0190
Naiemjoy1 <naiemunhasan@gmail.com>
2025-08-09 13:11:21 +0600

    type alias

---
+++ module/output/destructuring.js
@@ -0,0 +1,5 @@
+// array  destructuring
+const myFriends = ["a", "b", "c", "d"];
+const [e, f, g, ...rest] = myFriends;
+export {};
+//# sourceMappingURL=destructuring.js.map
---
+++ module/output/typeAlias.js
@@ -0,0 +1,22 @@
+const student1 = {
+    name: "Naiem",
+    age: 30,
+    gender: "male",
+    contactNo: "01703665364",
+    address: "srj",
+};
+const student2 = {
+    name: "aaafdgfg",
+    age: 40,
+    gender: "male",
+    address: "dhk",
+};
+const userName = "hhf";
+const isAdmin = true;
+console.log(student1);
+console.log(student2);
+console.log(userName);
+console.log(isAdmin);
+const add = (num1, num2) => num1 + num2;
+export {};
+//# sourceMappingURL=typeAlias.js.map
