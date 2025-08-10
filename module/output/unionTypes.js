bee8ccb1c25e7ebae5e3da21a8a25ff5c0be3792
Naiemjoy1 <naiemunhasan@gmail.com>
2025-08-10 12:43:12 +0600

    union

---
+++ module/output/unionTypes.js
@@ -0,0 +1,12 @@
+const fullStackDeveloper = {
+    skills: ["HTML", "CSS", "EXPRESS"],
+    designation1: "Frontend Developer",
+};
+const backendDeveloper = {
+    skills: ["HTML", "CSS", "EXPRESS"],
+    designation1: "Backend Developer",
+};
+console.log(fullStackDeveloper);
+console.log(backendDeveloper);
+export {};
+//# sourceMappingURL=unionTypes.js.map
